98d74122fba7f83046eb7d229986253b4cb5fa0b
jlnshly <jelaineashleyramos@gmail.com>
2026-06-04 03:44:11 +0800

    initial code defining main

---
+++ pet_demo.py
@@ -0,0 +1,11 @@
+import time
+from pet import Pet
+
+
+def main():
+    print("=" * 50)
+    print(" 🐾 WELCOME TO THE PET REGISTRATION PORTAL 🐾 ")
+    print("=" * 50)
+
+    # Instantiate a blank Pet object first to show off the setter methods
+    my_pet = Pet()
